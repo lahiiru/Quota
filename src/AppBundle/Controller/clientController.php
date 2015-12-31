1c7cc606d7f8f5992dde4e6b2cdb47d4e2defa38
lahiiru <lahiiru@gmail.com>
2016-01-01 01:02:16 +0530

    dashboard/client/summarry first implementation

--- src/AppBundle/Controller/clientController.php
+++ src/AppBundle/Controller/clientController.php
@@ -5,18 +5,30 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\DTO;
+use AppBundle\Entity;
+
 class ClientController extends Controller
 {
 
     public function summaryAction(Request $request){
-        $html=$this->render('dashboard/client/summary.html.twig', array(
-            'x' => ''
-        ));
+        $cUser=$this->get('security.token_storage')->getToken()->getUser();
+        $cId=$cUser->getId();
+        $em = $this->getDoctrine()->getManager();
 
+        $query = $em->createQuery("SELECT p FROM AppBundle\Entity\data_package p WHERE p.start < CURRENT_TIMESTAMP() AND CURRENT_TIMESTAMP() < p.end AND p.auth_user=$cId");
+
+        $cPackage = $query->getResult()[0];
+        $pstart=$cPackage->getStart();
+        $pend=$cPackage->getEnd();
+
+        $query = $em->createQuery("SELECT NEW AppBundle\DTO\ClientSummaryDTO(su.sid,su.name,su.mac,su.state,su.package,SUM(u.kbytes)) FROM AppBundle\Entity\slave_usage u JOIN u.slave_user su JOIN su.auth_user au WHERE au.id=$cId GROUP BY su");
+        $clientSummaryDTO = $query->getResult();
+        //var_dump($clientSummaryDTO);
+
+        $html=$this->render('dashboard/client/summary.html.twig', array(
+            'arrayDTO'=>$clientSummaryDTO,
+        ));
         return $html;
     }
 
